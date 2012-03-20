de30b96d87e7bcaa257f38155271de8e180e1260
Nick Doiron <nickd@codeforamerica.org>
2012-03-20 10:30:18 -0700

    cutting back to just transit code

--- lib/pdf_archive.rb
+++ lib/pdf_archive.rb
@@ -8,7 +8,7 @@ require 'uri'
 require 'net/http'
 
 # Application module
-module PdfArchive
+module Transit2Me
   def self.environment
     ENV['RACK_ENV'] || 'development'
   end
@@ -19,7 +19,7 @@ module PdfArchive
 end
 
 # MongoMapper setup
-mongo_url = ENV['MONGOHQ_URL'] || ENV['MONGOLAB_URI'] || "mongodb://localhost:27017/pdf_archive-#{PdfArchive.environment}"
+mongo_url = ENV['MONGOHQ_URL'] || ENV['MONGOLAB_URI'] || "mongodb://localhost:27017/pdf_archive-#{Transit2Me.environment}"
 uri = URI.parse(mongo_url)
 database = uri.path.gsub('/', '')
 MongoMapper.connection = Mongo::Connection.new(uri.host, uri.port, {})
@@ -43,10 +43,10 @@ CarrierWave.configure do |config|
 end
 
 # Grim Production Config
-if PdfArchive.environment == "production"
+if Transit2Me.environment == "production"
   Grim.processor = Grim::MultiProcessor.new([
-    Grim::ImageMagickProcessor.new({:ghostscript_path => PdfArchive.root.join('bin', '9.04', 'gs')}),
-    Grim::ImageMagickProcessor.new({:ghostscript_path => PdfArchive.root.join('bin', '9.02', 'gs')})
+    Grim::ImageMagickProcessor.new({:ghostscript_path => Transit2Me.root.join('bin', '9.04', 'gs')}),
+    Grim::ImageMagickProcessor.new({:ghostscript_path => Transit2Me.root.join('bin', '9.02', 'gs')})
   ])
 end
 
@@ -54,18 +54,11 @@ require 'exceptional'
 set :raise_errors, true
 use Rack::Exceptional, ENV['EXCEPTIONAL_API_KEY'] if ENV['RACK_ENV'] == 'production' && ENV['EXCEPTIONAL_API_KEY']
 
-# require pdf uploader, document model, and process pdf job
-require 'uploader'
-require 'pdf_uploader'
-require 'preview_store'
-require 'document'
-require 'process_pdf'
-
 # transit stuff
 require 'transitevent'
 
 # Routes
-set :public_folder, "#{PdfArchive.root}/public"
+set :public_folder, "#{Transit2Me.root}/public"
 
 class BartStation
    def initialize(id, latlng, name)
@@ -85,141 +78,51 @@ def bart_id_to_name(bid)
   stations = [
 	BartStation.new("19TH", [37.8076295249,-122.268869226], "19th St. Oakland" ),
 	BartStation.new("24TH",[37.752411,-122.418292],"24th St. Mission"),
-	BartStation.new("ASHB",
-		[37.853061,-122.269946],
-		"Ashby"),
-	BartStation.new("BALB",
-		[37.7219808677,-122.447414196],
-		"Balboa Park"),
-	BartStation.new("COLS",
-		[37.7542813804,-122.197788821],
-		"Coliseum/Oakland Airport"),
-		BartStation.new("CONC",
-		[37.9720158312,-122.029861348],
-		"Concord"),
-		BartStation.new("DALY",
-		[37.7061205485,-122.469080674],
-		"Daly City"),
-		BartStation.new("DBRK",
-		[37.8698684624,-122.268050932],
-		"Downtown Berkeley"),
-		BartStation.new("DELN",
-		[37.9256508785,-122.31721887],
-		"El Cerrito del Norte"),
-		BartStation.new("DUBL",
-		[37.7016736171,-121.900352519],
-		"Dublin/Pleasanton"),
-		BartStation.new("EMBR",
-		[37.7930224405,-122.396813153],
-		"Embarcadero"),
-		BartStation.new("FRMT",
-		[37.5573342821,-121.976395442],
-		"Fremont"),
-		BartStation.new("FTVL",
-		[37.7746238056,-122.224327698],
-		"Fruitvale"),
-		BartStation.new("GLEN",
-		[37.7329415443,-122.434114331],
-		"Glen Park"),
-		BartStation.new("HAYW",
-		[37.6703868939,-122.088002125],
-		"Hayward"),
-		BartStation.new("LAFY",
-		[37.8934254672,-122.123798472],
-		"Lafayette Park"),
-		BartStation.new("LAKE",
-		[37.7976023716,-122.265498391],
-		"Lake Merritt"),
-		BartStation.new("MCAR",
-		[37.8284094079,-122.267187102],
-		"MacArthur"),
-		BartStation.new("MONT",
-		[37.7893359611,-122.401485489],
-		"Montgomery St."),
-		BartStation.new("NBRK",
-		[37.87402614,-122.283881911],
-		"North Berkeley"),
-		BartStation.new("NCON",
-		[38.002576647,-122.025106029],
-		"North Concord/Martinez"),
-		BartStation.new("ORIN",
-		[37.8783608699,-122.183791135],
-		"Orinda"),
-		BartStation.new("PHIL",
-		[37.9277362737,-122.056847034],
-		"Pleasant Hill/Contra Costa Centre"),
-		BartStation.new("PITT",
-		[38.0189343386,-121.941904488],
-		"Pittsburg/Bay Point"),
-		BartStation.new("PLZA",
-		[37.9030588009,-122.29927151],
-		"El Cerrito Plaza"),
-		BartStation.new("BAYF",
-		[37.6978321791,-122.127858508],
-		"Bay Fair"),
-		BartStation.new("CAST",
-		[37.6907303057,-122.077460002],
-		"Castro Valley"),
-		BartStation.new("CIVC",
-		[37.7796055874,-122.413851084],
-		"Civic Center/UN Plaza"),
-		BartStation.new("19TH_N",
-		[37.8076295249,-122.268869226],
-		"19th St. Oakland"),
-		BartStation.new("WDUB",
-		[37.6998,-121.9281],
-		"West Dublin/Pleasanton"),
-		BartStation.new("POWL",
-		[37.7849710021,-122.407012285],
-		"Powell St."),
-		BartStation.new("ROCK",
-		[37.8441838514,-122.252731522],
-		"Rockridge"),
-		BartStation.new("SANL",
-		[37.7226192073,-122.161311154],
-		"San Leandro"),
-		BartStation.new("SHAY",
-		[37.6347995391,-122.057550587],
-		"South Hayward"),
-		BartStation.new("SSAN",
-		[37.66433,-122.44399],
-		"South San Francisco"),
-		BartStation.new("UCTY",
-		[37.591202687,-122.017857962],
-		"Union City"),
-		BartStation.new("WCRK",
-		[37.9046388803,-122.068018135],
-		"Walnut Creek"),
-		BartStation.new("WOAK",
-		[37.8046747595,-122.294582214],
-		"West Oakland"),
-		BartStation.new("COLM",
-		[37.6845808931,-122.467369242],
-		"Colma"),
-		BartStation.new("MLBR",
-		[37.600006,-122.386534],
-		"Millbrae"),
-		BartStation.new("RICH",
-		[37.9371699076,-122.3534001],
-		"Richmond"),
-		BartStation.new("SBRN",
-		[37.637143,-122.415912],
-		"San Bruno"),
-		BartStation.new("SFIA",
-		[37.6159,-122.392534],
-		"San Francisco Int"),
-		BartStation.new("OAK",
-		[37.712811,-122.213165],
-		"Oakland Airport Terminals"),
-		BartStation.new("MCAR_S",
-		[37.8284094079,-122.267187102],
-		"MacArthur"),
-		BartStation.new("12TH",
-		[37.8030927157,-122.271655014],
-		"12th St. Oakland City Center"),
-	BartStation.new("16TH",
-		[37.765228,-122.419478],
-		"16th St. Mission")
+	BartStation.new("ASHB",[37.853061,-122.269946],"Ashby"),
+	BartStation.new("BALB",[37.7219808677,-122.447414196],"Balboa Park"),
+	BartStation.new("COLS",[37.7542813804,-122.197788821],"Coliseum/Oakland Airport"),
+		BartStation.new("CONC",[37.9720158312,-122.029861348],"Concord"),
+		BartStation.new("DALY",[37.7061205485,-122.469080674],"Daly City"),
+		BartStation.new("DBRK",[37.8698684624,-122.268050932],"Downtown Berkeley"),
+		BartStation.new("DELN",[37.9256508785,-122.31721887],"El Cerrito del Norte"),
+		BartStation.new("DUBL",[37.7016736171,-121.900352519],"Dublin/Pleasanton"),
+		BartStation.new("EMBR",[37.7930224405,-122.396813153],"Embarcadero"),
+		BartStation.new("FRMT",[37.5573342821,-121.976395442],"Fremont"),
+		BartStation.new("FTVL",[37.7746238056,-122.224327698],"Fruitvale"),
+		BartStation.new("GLEN",[37.7329415443,-122.434114331],"Glen Park"),
+		BartStation.new("HAYW",[37.6703868939,-122.088002125],"Hayward"),
+		BartStation.new("LAFY",[37.8934254672,-122.123798472],"Lafayette Park"),
+		BartStation.new("LAKE",[37.7976023716,-122.265498391],"Lake Merritt"),
+		BartStation.new("MCAR",[37.8284094079,-122.267187102],"MacArthur"),
+		BartStation.new("MONT",[37.7893359611,-122.401485489],"Montgomery St."),
+		BartStation.new("NBRK",[37.87402614,-122.283881911],"North Berkeley"),
+		BartStation.new("NCON",[38.002576647,-122.025106029],"North Concord/Martinez"),
+		BartStation.new("ORIN",[37.8783608699,-122.183791135],"Orinda"),
+		BartStation.new("PHIL",[37.9277362737,-122.056847034],"Pleasant Hill/Contra Costa Centre"),
+		BartStation.new("PITT",[38.0189343386,-121.941904488],"Pittsburg/Bay Point"),
+		BartStation.new("PLZA",[37.9030588009,-122.29927151],"El Cerrito Plaza"),
+		BartStation.new("BAYF",[37.6978321791,-122.127858508],"Bay Fair"),
+		BartStation.new("CAST",[37.6907303057,-122.077460002],"Castro Valley"),
+		BartStation.new("CIVC",[37.7796055874,-122.413851084],"Civic Center/UN Plaza"),
+		BartStation.new("19TH_N",[37.8076295249,-122.268869226],"19th St. Oakland"),
+		BartStation.new("WDUB",[37.6998,-121.9281],"West Dublin/Pleasanton"),
+		BartStation.new("POWL",[37.7849710021,-122.407012285],"Powell St."),
+		BartStation.new("ROCK",[37.8441838514,-122.252731522],"Rockridge"),
+		BartStation.new("SANL",[37.7226192073,-122.161311154],"San Leandro"),
+		BartStation.new("SHAY",[37.6347995391,-122.057550587],"South Hayward"),
+		BartStation.new("SSAN",[37.66433,-122.44399],"South San Francisco"),
+		BartStation.new("UCTY",[37.591202687,-122.017857962],"Union City"),
+		BartStation.new("WCRK",[37.9046388803,-122.068018135],"Walnut Creek"),
+		BartStation.new("WOAK",[37.8046747595,-122.294582214],"West Oakland"),
+		BartStation.new("COLM",[37.6845808931,-122.467369242],"Colma"),
+		BartStation.new("MLBR",[37.600006,-122.386534],"Millbrae"),
+		BartStation.new("RICH",[37.9371699076,-122.3534001],"Richmond"),
+		BartStation.new("SBRN",[37.637143,-122.415912],"San Bruno"),
+		BartStation.new("SFIA",[37.6159,-122.392534],"San Francisco Int"),
+		BartStation.new("OAK",[37.712811,-122.213165],"Oakland Airport Terminals"),
+		BartStation.new("MCAR_S",[37.8284094079,-122.267187102],"MacArthur"),
+		BartStation.new("12TH",[37.8030927157,-122.271655014],"12th St. Oakland City Center"),
+	BartStation.new("16TH",[37.765228,-122.419478],"16th St. Mission")
   ]
   stations.each do |station|
     if station.getid() == bid
@@ -311,19 +214,10 @@ post '/transit' do
 end
 
 get '/' do
-  erb :home
+  erb :transit
 end
 
-post '/' do
-  if params['pdf']
-    document = Document.create!(params)
-    Qu.enqueue(ProcessPdf, document.id)
-  end
-
-  erb :home
-end
-
-get '/search' do
-  @documents = Document.search(params['q'])
-  erb :home
-end
+#get '/search' do
+#  @documents = Document.search(params['q'])
+#  erb :home
+#end
