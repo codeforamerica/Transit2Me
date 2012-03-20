50052cd5379ea72df95ccf9c656e2259c2332fd2
Nick Doiron <nickd@codeforamerica.org>
2012-03-20 10:55:07 -0700

    sending narrative to event posting

--- lib/pdf_archive.rb
+++ lib/pdf_archive.rb
@@ -201,7 +201,7 @@ get '/transit' do
         carbon = bartschedule.slice( bartschedule.index('strong') + 7 .. bartschedule.index('pounds') - 2 )
       end
     end
-    erb :transitposted, :locals => { :narrative => narrative, :carbon => carbon }
+    erb :transitposted, :locals => { :narrative => url, :carbon => carbon }
   else
     erb :transit
   end
