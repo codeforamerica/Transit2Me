baf51b3a5232da23e0f59a2a8d0e3fc48c53402b
Nick Doiron <nickd@codeforamerica.org>
2012-03-20 11:21:43 -0700

    setting up geocoding thru MapQuest

--- lib/pdf_archive.rb
+++ lib/pdf_archive.rb
@@ -72,6 +72,74 @@ class BartStation
    def getname()
      return @name
    end
+   def getlat()
+     return @latlng[0]
+   end
+   def getlng()
+     return @latlng[1]
+   end
+end
+
+def closest_bart(lat, lng)
+  stations = [
+	BartStation.new("19TH", [37.8076295249,-122.268869226], "19th St. Oakland" ),
+	BartStation.new("24TH",[37.752411,-122.418292],"24th St. Mission"),
+	BartStation.new("ASHB",[37.853061,-122.269946],"Ashby"),
+	BartStation.new("BALB",[37.7219808677,-122.447414196],"Balboa Park"),
+	BartStation.new("COLS",[37.7542813804,-122.197788821],"Coliseum/Oakland Airport"),
+		BartStation.new("CONC",[37.9720158312,-122.029861348],"Concord"),
+		BartStation.new("DALY",[37.7061205485,-122.469080674],"Daly City"),
+		BartStation.new("DBRK",[37.8698684624,-122.268050932],"Downtown Berkeley"),
+		BartStation.new("DELN",[37.9256508785,-122.31721887],"El Cerrito del Norte"),
+		BartStation.new("DUBL",[37.7016736171,-121.900352519],"Dublin/Pleasanton"),
+		BartStation.new("EMBR",[37.7930224405,-122.396813153],"Embarcadero"),
+		BartStation.new("FRMT",[37.5573342821,-121.976395442],"Fremont"),
+		BartStation.new("FTVL",[37.7746238056,-122.224327698],"Fruitvale"),
+		BartStation.new("GLEN",[37.7329415443,-122.434114331],"Glen Park"),
+		BartStation.new("HAYW",[37.6703868939,-122.088002125],"Hayward"),
+		BartStation.new("LAFY",[37.8934254672,-122.123798472],"Lafayette Park"),
+		BartStation.new("LAKE",[37.7976023716,-122.265498391],"Lake Merritt"),
+		BartStation.new("MCAR",[37.8284094079,-122.267187102],"MacArthur"),
+		BartStation.new("MONT",[37.7893359611,-122.401485489],"Montgomery St."),
+		BartStation.new("NBRK",[37.87402614,-122.283881911],"North Berkeley"),
+		BartStation.new("NCON",[38.002576647,-122.025106029],"North Concord/Martinez"),
+		BartStation.new("ORIN",[37.8783608699,-122.183791135],"Orinda"),
+		BartStation.new("PHIL",[37.9277362737,-122.056847034],"Pleasant Hill/Contra Costa Centre"),
+		BartStation.new("PITT",[38.0189343386,-121.941904488],"Pittsburg/Bay Point"),
+		BartStation.new("PLZA",[37.9030588009,-122.29927151],"El Cerrito Plaza"),
+		BartStation.new("BAYF",[37.6978321791,-122.127858508],"Bay Fair"),
+		BartStation.new("CAST",[37.6907303057,-122.077460002],"Castro Valley"),
+		BartStation.new("CIVC",[37.7796055874,-122.413851084],"Civic Center/UN Plaza"),
+		BartStation.new("19TH_N",[37.8076295249,-122.268869226],"19th St. Oakland"),
+		BartStation.new("WDUB",[37.6998,-121.9281],"West Dublin/Pleasanton"),
+		BartStation.new("POWL",[37.7849710021,-122.407012285],"Powell St."),
+		BartStation.new("ROCK",[37.8441838514,-122.252731522],"Rockridge"),
+		BartStation.new("SANL",[37.7226192073,-122.161311154],"San Leandro"),
+		BartStation.new("SHAY",[37.6347995391,-122.057550587],"South Hayward"),
+		BartStation.new("SSAN",[37.66433,-122.44399],"South San Francisco"),
+		BartStation.new("UCTY",[37.591202687,-122.017857962],"Union City"),
+		BartStation.new("WCRK",[37.9046388803,-122.068018135],"Walnut Creek"),
+		BartStation.new("WOAK",[37.8046747595,-122.294582214],"West Oakland"),
+		BartStation.new("COLM",[37.6845808931,-122.467369242],"Colma"),
+		BartStation.new("MLBR",[37.600006,-122.386534],"Millbrae"),
+		BartStation.new("RICH",[37.9371699076,-122.3534001],"Richmond"),
+		BartStation.new("SBRN",[37.637143,-122.415912],"San Bruno"),
+		BartStation.new("SFIA",[37.6159,-122.392534],"San Francisco Int"),
+		BartStation.new("OAK",[37.712811,-122.213165],"Oakland Airport Terminals"),
+		BartStation.new("MCAR_S",[37.8284094079,-122.267187102],"MacArthur"),
+		BartStation.new("12TH",[37.8030927157,-122.271655014],"12th St. Oakland City Center"),
+		BartStation.new("16TH",[37.765228,-122.419478],"16th St. Mission")
+  ]
+  closestStation = nil
+  closestDistance = 100000
+  stations.each do |station|
+    dist = (station.getlat() - lat )**2 + ( station.getlng() - lng )**2
+    if(dist < closestDistance)
+      closestDistance = dist
+      closestStation = station
+    end
+  end
+  return closestStation.getid()
 end
 
 def bart_id_to_name(bid)
@@ -130,6 +198,29 @@ def bart_id_to_name(bid)
     end
   end
   return bid
+end
+
+get '/geotransit' do
+  erb :georequest
+end
+
+post '/geotransit' do
+  if params['address']
+    url = 'http://www.mapquestapi.com/geocoding/v1/address?key=Fmjtd%7Cluua2l07nq%2C22%3Do5-hyy0g&location=' + params['address']
+    url = URI.parse(url)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.get('/geocoding/v1/address?key=Fmjtd%7Cluua2l07nq%2C22%3Do5-hyy0g&location=' + params['address'])
+    }
+    response = res.body
+    lng = response.slice( response.index('"lng":') + 6 .. response.index('"lat"') - 2 )
+    lng = Float(lng)
+    lat = response.slice( response.index('"lat":') + 6 .. response.length )
+    lat = lat.slice( 0 .. lat.index('}') - 1 )
+    lat = Float(lat)
+    closest = closest_bart(lat, lng)
+    closest
+  else
+    "no address"
 end
 
 get '/transit' do
